1658bba857f7d3a1e771a168bbd637c2136faeb6
Konstantin Alekseev <mail@kalekseev.com>
2022-08-02 21:47:40 +0300

    Trigger build

--- postgres/non-durable-settings.sh
+++ postgres/non-durable-settings.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
-# setting non-durable options
 # https://www.postgresql.org/docs/current/static/non-durability.html
-echo "Configuring postgres non-durable options."
-# no need to flush data to disk.
-echo "fsync = off" >> /var/lib/postgresql/data/postgresql.conf
-# no need to force WAL writes to disk on every commit.
-echo "synchronous_commit = off" >> /var/lib/postgresql/data/postgresql.conf
-# no need to guard against partial page writes.
-echo "full_page_writes = off" >> /var/lib/postgresql/data/postgresql.conf
+cat <<EOF >> /var/lib/postgresql/data/postgresql.conf
+fsync = off
+synchronous_commit = off
+full_page_writes = off
+EOF
