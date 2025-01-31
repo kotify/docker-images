cc2125cfd7ba039f75ca08e5f9aa3f6ef0326719
Konstantin Alekseev <konstantin@kotify.com>
2025-01-31 10:43:42 +0200

    increase wal size and max locks per transacton

--- postgres/non-durable-settings.sh
+++ postgres/non-durable-settings.sh
@@ -4,6 +4,10 @@ cat <<EOF >> /var/lib/postgresql/data/postgresql.conf
 fsync = off
 synchronous_commit = off
 full_page_writes = off
+max_wal_size = '4GB'
+checkpoint_timeout = '1d'
+
+max_locks_per_transaction = 128
 wal_level = minimal
 max_wal_senders = 0
 EOF
